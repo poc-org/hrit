d8595e861e825d68d2862b0ca995f19902735a6d
cadriosula <cfa_44@yahoo.com>
2018-01-10 23:39:19 +0800

    Update and rename init.js to initDeepLink.js

--- learning_guide/js/initDeepLink.js
+++ learning_guide/js/initDeepLink.js
@@ -1,8 +1,6 @@
-<script>  
 window.onload = function replaceLink() {
 	var links = document.querySelectorAll('a');
 	for (var i = 0; i < links.length; i++) {	
 		links[i].href = links[i].href.replace('https://dxc.sabacloud.com',"https://dxc-itg.sabacloud.com");
 	}
 }
-</script>
