a49a3f22e5dbf9e0a47d94591fd4d6fdd37089a3
cadriosula <cfa_44@yahoo.com>
2018-01-10 23:29:28 +0800

    Add files via upload

---
+++ learning_guide/js/init.js
@@ -0,0 +1,8 @@
+<script>  
+window.onload = function replaceLink() {
+	var links = document.querySelectorAll('a');
+	for (var i = 0; i < links.length; i++) {	
+		links[i].href = links[i].href.replace('https://dxc.sabacloud.com',"https://dxc-itg.sabacloud.com");
+	}
+}
+</script>
