aa1d7e3855ad4fd9fb6cf730783b0c597a5a3d97
cadriosula <cfa_44@yahoo.com>
2018-01-11 00:19:19 +0800

    Update vars.js

--- learning_guide/js/vars.js
+++ learning_guide/js/vars.js
@@ -1,4 +1,5 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-    $scope.url_home = "https://rawgit.com/poc-org/hrit/test";
+    //$scope.url_home = "https://rawgit.com/poc-org/hrit/test"; ITG
+    $scope.url_home = "https://rawgit.com/poc-org/hrit/";
 });
