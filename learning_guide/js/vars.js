938082ce79a3688bbfedf16f943c71bc9625a281
Christopher Moore <moore.christopher515@gmail.com>
2018-01-23 19:59:00 -0600

    Reorganization

--- learning_guide/js/vars.js
+++
@@ -1,4 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.url_home = "https://rawgit.com/poc-org/hrit/test";
-});
---
+++ learning_guide/res/js/header.js
@@ -0,0 +1,11 @@
+// JScript source code
+var headertext = '<div style="position: relative; top: 0px; left: 0px; padding-left: 0; background-color: #000000">'
+headertext += '\n<div style="position:relative;top:10px;left:0px;padding-left:20;background-color:#000000" >'
+headertext += '\n<img src="../res/images/dxc_logo.jpg">'
+headertext += '\n</div> '
+headertext += '\n<div style="position:absolute;top:0px;left:100px;padding-left:0;background-color:#000000">'
+headertext += '\n<h2 style="color:white;">DXC University</h3>'
+headertext += '\n</div>'
+headertext += '\n</div>'
+document.write(headertext);
+
