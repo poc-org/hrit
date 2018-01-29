682aa2e321a6466d5c235a02f773a795055351eb
Christopher Moore <christopher.jor.moore@hpe.com>
2018-01-29 12:32:30 -0600

    Revert "revert"
    This reverts commit cf4235ca2b5159eebdcdc3c9096a8ee921b3b324.

--- learning_guide/res/js/vars.js
+++ learning_guide/res/js/vars.js
@@ -1,4 +1,6 @@
 var app = angular.module('myApp', []);
+var data = JSON.parse(order);
+alert(data[0].topfirst);
 app.controller('myCtrl', function($scope) {
     $scope.url_home = "https://rawgit.com/poc-org/hrit/test";
 });
