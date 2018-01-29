cf4235ca2b5159eebdcdc3c9096a8ee921b3b324
Christopher Moore <christopher.jor.moore@hpe.com>
2018-01-29 12:27:06 -0600

    revert

--- learning_guide/res/js/vars.js
+++ learning_guide/res/js/vars.js
@@ -1,6 +1,4 @@
 var app = angular.module('myApp', []);
-var data = JSON.parse(order);
-alert(data[0].topfirst);
 app.controller('myCtrl', function($scope) {
     $scope.url_home = "https://rawgit.com/poc-org/hrit/test";
 });
