3b60a211daed63b237a25f934c39631ed58cda77
Christopher Moore <christopher.jor.moore@hpe.com>
2018-01-29 14:40:34 -0600

    testing home page changing

--- learning_guide/res/js/vars.js
+++ learning_guide/res/js/vars.js
@@ -1,4 +1,8 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
     $scope.url_home = "https://rawgit.com/poc-org/hrit/test";
+	$scope.location = function(page){
+		var pages = JSON.parse(order);
+		return pages[page].name;
+	}
 });
