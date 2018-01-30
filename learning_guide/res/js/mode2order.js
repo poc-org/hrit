ed41011e324b40baa60ff50b8a0bcc2b94bfeddb
ChristopherDXC <35551612+ChristopherDXC@users.noreply.github.com>
2018-01-30 22:48:06 +0000

    Update mode2order.js

--- learning_guide/res/js/mode2order.js
+++ learning_guide/res/js/mode2order.js
@@ -1,9 +1,10 @@
 var conf = JSON.parse(order);
 
 var table = document.getElementById("table");
-table.innerHTML = '<table><tbody>';
+var html = '<table><tbody>';
 for (var index = 0; index < conf.length; ++index){
- table.innerHTML += '<tr style="height: 33px;"><td valign="top"><a href="{{url_home}}/learning_guide/pages/{{modeorder(' + index + ')}}/{{modeorder(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{modeorder(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>'
+ html += '<tr style="height: 33px;"><td valign="top"><a href="{{url_home}}/learning_guide/pages/{{modeorder(' + index + ')}}/{{modeorder(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{modeorder(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
 }
-table.innerHTML += '</tbody></table>'
+html += '</tbody></table>';
+tabl.innerHTML = html;
 
