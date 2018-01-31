646f04ae6a1ac48e12f7bed1ebf2f9f3f9d667f8
Christopher Moore <christopher.jor.moore@hpe.com>
2018-01-30 18:14:19 -0600

    force tables to be 8 depth

--- learning_guide/res/js/mode2order.js
+++ learning_guide/res/js/mode2order.js
@@ -1,9 +1,19 @@
 var json = JSON.parse(conf);
 
 var table = document.getElementById("table");
-var html = '<table><tbody>';
+var html = '';
+first = true;
 for (var index = 0; index < json.length; ++index){
- html += '<tr style="height: 33px;"><td valign="top"><a href="{{url_home}}/learning_guide/pages/{{modeorder(' + index + ')}}/{{modeorder(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{modeorder(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
+	if(index % 8 == 0){
+		if (first == true){
+			first = false;
+			html += '<table style="float:left" style="padding-right:200px"><tbody><tr style="height: 33px;"><td valign="top"><a href="{{url_home}}/learning_guide/pages/{{modeorder(' + index + ')}}/{{modeorder(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{modeorder(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
+		} else {
+			html += '</tbody></table><table style="float:left" style="padding-right:200px"><tbody><tr style="height: 33px;"><td valign="top"><a href="{{url_home}}/learning_guide/pages/{{modeorder(' + index + ')}}/{{modeorder(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{modeorder(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
+		}
+	} else {
+		html += '<tr style="height: 33px;"><td valign="top"><a href="{{url_home}}/learning_guide/pages/{{modeorder(' + index + ')}}/{{modeorder(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{modeorder(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
+	}
 }
 html += '</tbody></table>';
 table.innerHTML = html;
