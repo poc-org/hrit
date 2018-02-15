cc8ec6f126d50d780a3434ff3f29acf4386b7cc1
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-15 09:56:43 +0800

    Modified according to tool

--- learning_guide/res/js/mode2order.js
+++ learning_guide/res/js/mode2order.js
@@ -7,12 +7,12 @@ for (var index = 0; index < json.length; ++index){
 	if(index % 8 == 0){
 		if (first == true){
 			first = false;
-			html += '<table style="float:left;padding-right:150px;"><tbody><tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/{{modeorder(' + index + ')}}/{{modeorder(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{modeorder(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
+			html += '<table style="float:left;padding-right:150px;"><tbody><tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/{{location(' + index + ')}}/{{location(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{location(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
 		} else {
-			html += '</tbody></table><table style="float:left;padding-right:150px;"><tbody><tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/{{modeorder(' + index + ')}}/{{modeorder(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{modeorder(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
+			html += '</tbody></table><table style="float:left;padding-right:150px;"><tbody><tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/{{location(' + index + ')}}/{{location(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{location(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
 		}
 	} else {
-		html += '<tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/{{modeorder(' + index + ')}}/{{modeorder(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{modeorder(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
+		html += '<tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/{{location(' + index + ')}}/{{location(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{location(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
 	}
 }
 html += '</tbody></table>';
