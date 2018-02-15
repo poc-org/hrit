d5d597d6e9bd75ebf06ca5f1d85d88baeb75f549
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-15 09:54:27 +0800

    Modified according to the tool

--- learning_guide/res/js/vars.js
+++ learning_guide/res/js/vars.js
@@ -2,11 +2,11 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
     $scope.url_home = "https://rawgit.com/poc-org/hrit/test";
 	$scope.location = function(page){
-		var pages = JSON.parse(order);
-		return pages[page].name;
-	};
-	$scope.modeorder = function(page){
 		var pages = JSON.parse(conf);
 		return pages[page].name;
+	};
+	$scope.mode2conf = function(){
+		var pages = JSON.parse(mode2conf);
+		return pages[0].name;
 	};
 });
