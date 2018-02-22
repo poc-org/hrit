18da496328a2602be3f3ad8b210f3a099807f731
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-22 14:52:57 +0800

    Update guidedlearning.js

--- learning_guide/res/js/guidedlearning.js
+++ learning_guide/res/js/guidedlearning.js
@@ -16,20 +16,20 @@ for (var index = 0; index < lengthofLoop; ++index) {
         html += '</tr><tr>'; // Add row separator
     }
     if ((json[index].mode) == 2){
-        html += '<td valign="top"><a href="{{url_mode2}}' + json[index].name + '" target="_blank"><img src="{{url_image}}' + json[index].name + '.jpg" class="imgBox" /></a></td>';
+        html += '<td valign="top"><a href="' + json[index].name + '" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/04.jpg" class="imgBox" /></a></td>';
     }
     else if ((json[index].mode) == 3) {
-        html += '<td valign="top"><a href="' + json[index].url + '" target="_blank"><img src="{{url_image}}' + json[index].name + '.jpg" class="imgBox" /></a></td>';
+        html += '<td valign="top"><a href="' + json[index].url + '" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/03.jpg" class="imgBox" /></a></td>';
     }
     else {
-        html += '<td valign="top"><a href="{{url_home}}/learning_guide/pages/' + json[index].name + '/' + json[index].name + '.html" target="_blank"><img src="{{url_image}}' + json[index].name + '.jpg" class="imgBox" /></a></td>';
+        html += '<td valign="top"><a href="' + json[index].name + '/' + json[index].name + '.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/02.jpg" class="imgBox" /></a></td>';
     }
 }
 
 //Added for the fixed cell for more 
 if (lengthofLoop > 8) {
     
-    html += '<td valign="top"><a href="{{url_mode2}}conf" target="_blank"><img src="{{url_image}}{{mode2conf()}}.jpg" class="imgBox" /></a></td>';
+    html += '<td valign="top"><a href="conf" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/01.jpg" class="imgBox" /></a></td>';
 }
 html += '</tr></table>';
 console.log(html);
