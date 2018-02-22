99152349612149e1b3d1f2172cfad25e998030b8
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-22 16:50:34 +0800

    Update vars.js

--- learning_guide/res/js/vars.js
+++ learning_guide/res/js/vars.js
@@ -1,6 +1,6 @@
  var url_home = "https://rawgit.com/poc-org/hrit/test";
  var url_image = "https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/";
- var url_mode2 = "https://rawgit.com/poc-org/hrit/test/pages/mod2page/mod2page.html?page=";
+ var url_mode2 = "https://rawgit.com/poc-org/hrit/test/learning_guide/pages/mode2page/mod2page.html?page";
  var mode2Image = "10.jpg";
     
 
