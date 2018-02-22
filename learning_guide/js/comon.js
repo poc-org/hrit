e57032164242c366110ac8084d2ea9ec235e4d5f
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-22 13:44:29 +0800

    Added for the common function for all application
    Added for the common function for all application

---
+++ learning_guide/js/comon.js
@@ -0,0 +1,49 @@
+// Get Query String Values Start 
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+
+    return vars;
+
+}
+// Get Query String Values END
+//Load Dynamically JS file in page start 
+function loadScript(url, callback) {
+
+    var script = document.createElement("script")
+    script.type = "text/javascript";
+
+    if (script.readyState) {  //IE
+        script.onreadystatechange = function () {
+            if (script.readyState == "loaded" ||
+                    script.readyState == "complete") {
+                script.onreadystatechange = null;
+                // callback();
+            }
+        };
+    } else {  //Others
+        script.onload = function () {
+            //callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+    //loadjscssfile(varConf)
+        function loadjscssfile(filename, filetype) {           
+                var fileref = document.createElement('script')
+                fileref.setAttribute("type", "text/javascript")
+                fileref.setAttribute("src", varConf)
+                
+            if (typeof fileref != "undefined")
+                document.getElementsByTagName("head")[0].appendChild(fileref)
+        }
+//Load Dynamically JS file in page END
