2c5f4577f93d001139bb41743c1b0e2ca6bec69f
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-22 15:06:22 +0800

    Update guidedlearning.js

--- learning_guide/res/js/guidedlearning.js
+++ learning_guide/res/js/guidedlearning.js
@@ -1,6 +1,6 @@
 var json = JSON.parse(conf);
 var table = document.getElementById("tblGuidedLearning");
-var html = '';
+var htmlData = '';
 //alert(url_home);
 first = true;
 var lengthofLoop;
@@ -10,28 +10,29 @@ if (json.length > 9) {
 else {
     lengthofLoop = json.length
 }
-var html = '<table><tr>';
+var htmlData = '<table><tr>';
 for (var index = 0; index < lengthofLoop; ++index) {
     if ((index == 5) || (index == 10)) { // Start of next row
-        html += '</tr><tr>'; // Add row separator
+        htmlData += '</tr><tr>'; // Add row separator
     }
     if ((json[index].mode) == 2){
-        html += '<td valign="top"><a href="' + json[index].name + '" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/04.jpg" class="imgBox" /></a></td>';
+        htmlData += '<td valign="top"><a href="' + json[index].name + '" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/04.jpg" class="imgBox" /></a></td>';
     }
     else if ((json[index].mode) == 3) {
-        html += '<td valign="top"><a href="' + json[index].url + '" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/03.jpg" class="imgBox" /></a></td>';
+        htmlData += '<td valign="top"><a href="' + json[index].url + '" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/03.jpg" class="imgBox" /></a></td>';
     }
     else {
-        html += '<td valign="top"><a href="' + json[index].name + '/' + json[index].name + '.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/02.jpg" class="imgBox" /></a></td>';
+        htmlData += '<td valign="top"><a href="' + json[index].name + '/' + json[index].name + '.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/02.jpg" class="imgBox" /></a></td>';
     }
 }
 
 //Added for the fixed cell for more 
 if (lengthofLoop > 8) {
     
-    html += '<td valign="top"><a href="conf" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/01.jpg" class="imgBox" /></a></td>';
+    htmlData += '<td valign="top"><a href="conf" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/01.jpg" class="imgBox" /></a></td>';
 }
-html += '</tr></table>';
-console.log(html);
-table.innerHTML = html;
+htmlData += '</tr></table>';
+console.log(htmlData);
+//table.innerHTML = html;
+$("#tblGuidedLearning").html(htmlData);
 
