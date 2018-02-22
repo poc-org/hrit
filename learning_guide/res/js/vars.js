c10b5e2e89fb2de3fee191501297741bd11b465b
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-22 13:53:04 +0800

    Enhanced as per the new requirement

--- learning_guide/res/js/vars.js
+++ learning_guide/res/js/vars.js
@@ -1,12 +1,19 @@
+
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
+app.controller('myCtrl', function ($scope) {
+    
     $scope.url_home = "https://rawgit.com/poc-org/hrit/test";
-	$scope.location = function(page){
-		var pages = JSON.parse(conf);
-		return pages[page].name;
-	};
-	$scope.mode2conf = function(){
-		var pages = JSON.parse(mode2conf);
-		return pages[0].name;
-	};
+    $scope.url_image = "https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/";
+    $scope.url_mode2 = "https://rawgit.com/poc-org/hrit/test/mod2page.html?page=";
+    
+    $scope.location = function (page) {
+        var pages = JSON.parse(conf);
+        return pages[page].name;
+    };
+    $scope.mode2conf = function () {
+        var pages = JSON.parse(mode2conf);
+        return pages[0].name;
+    };
 });
+
+
