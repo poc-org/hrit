7a52d93d8909eec64831f22f916f334716851754
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-22 13:51:33 +0800

    Enhanced as per new requirement
    Enhanced as per new requirement

--- learning_guide/res/js/mode2order.js
+++ learning_guide/res/js/mode2order.js
@@ -1,19 +1,18 @@
 var json = JSON.parse(conf);
-
 var table = document.getElementById("table");
 var html = '';
 first = true;
-for (var index = 0; index < json.length; ++index){
-	if(index % 8 == 0){
-		if (first == true){
-			first = false;
-			html += '<table style="float:left;padding-right:150px;"><tbody><tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/{{location(' + index + ')}}/{{location(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{location(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
-		} else {
-			html += '</tbody></table><table style="float:left;padding-right:150px;"><tbody><tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/{{location(' + index + ')}}/{{location(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{location(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
-		}
-	} else {
-		html += '<tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/{{location(' + index + ')}}/{{location(' + index + ')}}.html" target="_blank"><img src="https://raw.githubusercontent.com/poc-org/hrit/test/learning_guide/res/images/{{location(' + index + ')}}.jpg" class="imgBox" /></a></td></tr>';
-	}
+for (var index = 0; index < json.length; ++index) {
+    if (index % 8 == 0) {
+        if (first == true) {
+            first = false;
+            html += '<table style="float:left;padding-right:150px;"><tbody><tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/' + json[index].name + '/' + json[index].name + '.html" target="_blank"><img src="{{url_image}}' + json[index].name + '.jpg" class="imgBox" /></a></td></tr>';
+        } else {
+            html += '</tbody></table><table style="float:left;padding-right:150px;"><tbody><tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/' + json[index].name + '/' + json[index].name + '.html" target="_blank"><img src="{{url_image}}' + json[index].name + '.jpg" class="imgBox" /></a></td></tr>';
+        }
+    } else {
+        html += '<tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/' + json[index].name + '/' + json[index].name + '.html" target="_blank"><img src="{{url_image}}' + json[index].name + '.jpg" class="imgBox" /></a></td></tr>';
+    }
 }
 html += '</tbody></table>';
 table.innerHTML = html;
