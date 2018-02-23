d13699b1d579632b4202e2bca256863d6911fab6
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-23 16:19:55 +0800

    Add new tree struc

--- learning_guide/res/js/mode2order.js
+++ learning_guide/res/js/mode2order.js
@@ -1,20 +1,42 @@
-
 var json = JSON.parse(conf);
 var table = document.getElementById("table");
 var html = '';
+var varURLPage
 first = true;
 for (var index = 0; index < json.length; ++index) {
+    if ((json[index].mode) == 2) {
+        
+        varURLPage =  '<a href="' + url_mode2 + json[index].name + '" target="_blank">';
+
+    }
+    else if ((json[index].mode) == 3) {
+        
+        varURLPage = '<a href="' + json[index].url + '" target="_blank">';
+    }
+    else {
+                
+        varURLPage = '<a href="' + url_home + '/learning_guide/pages/' + json[index].name + '/' + json[index].name + '.html" target="_blank">';
+
+    }
+
     if (index % 8 == 0) {
         if (first == true) {
             first = false;
-            html += '<table style="float:left;padding-right:150px;"><tbody><tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/' + json[index].name + '/' + json[index].name + '.html" target="_blank"><img src="{{url_image}}' + json[index].name + '.jpg" class="imgBox" /></a></td></tr>';
+            html += '<table style="float:left;padding-right:150px;"><tbody><tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span>';
+            html += varURLPage;
+            html += '<img src="' + url_image + json[index].name + '.jpg" class="imgBox" /></a></td></tr>';
         } else {
-            html += '</tbody></table><table style="float:left;padding-right:150px;"><tbody><tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/' + json[index].name + '/' + json[index].name + '.html" target="_blank"><img src="{{url_image}}' + json[index].name + '.jpg" class="imgBox" /></a></td></tr>';
+            html += '</tbody></table><table style="float:left;padding-right:150px;"><tbody><tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span>';
+            html += varURLPage;
+            html += '<img src="' + url_image + json[index].name + '.jpg" class="imgBox" /></a></td></tr>';
         }
     } else {
-        html += '<tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span><a href="{{url_home}}/learning_guide/pages/' + json[index].name + '/' + json[index].name + '.html" target="_blank"><img src="{{url_image}}' + json[index].name + '.jpg" class="imgBox" /></a></td></tr>';
+        html += '<tr style="height: 33px;"><td valign="top" class="tooltip"><span class="tooltiptext">' + json[index].description + '</span>';
+        html += varURLPage;
+        html += '<img src="' + url_image + json[index].name + '.jpg" class="imgBox" /></a></td></tr>';
     }
 }
 html += '</tbody></table>';
 table.innerHTML = html;
+
 
